1ecac337534c0b2966771df33d019e1eac81fd20
Jackson O'Donnell <jacksonhodonnell@gmail.com>
2018-05-08 16:27:14 -0500

    lexers should now report location

--- src/lexer.lisp
+++ src/lexer.lisp
@@ -12,6 +12,23 @@
   (intern (concatenate 'string "LEX-" (symbol-name lexer-name) "-" (symbol-name rule-name))
           *package*))
 
+(defun make-lexer-for-terminal (lexer-name rule)
+  (let* ((this-name (make-lexer-name lexer-name (car rule)))
+         (rule-name (car rule))
+         (tmp-name (gensym))
+         (res-name (gensym))
+         (loc-name (gensym))
+         (header (cons (list loc-name :location)
+                       (if (cddr rule)
+                           (cadr rule)
+                           `((,tmp-name ,(cadr rule)))))))
+    `(,this-name ()
+       (make-parser ',this-name ,header
+         (let ((,res-name ,(if (cddr rule)
+                               `(progn ,@(cddr rule))
+                               tmp-name)))
+           (values ,rule-name ,res-name ,loc-name))))))
+
 (defmacro deflexer (name &key documentation whitespace terminals)
   (let* ((terminal-names (mapcar #'(lambda (n) (the keyword (car n)))
                                  terminals))
@@ -19,30 +36,22 @@
                               terminal-names))
          (parser-core `(alternative
                         ,@(mapcar #'(lambda (name) `(,name))
-                                  lexer-names))))
+                                              lexer-names))))
     `(eval-when (:compile-toplevel :load-toplevel)
        (labels
            ,(mapcar #'(lambda (rule)
-                        (let ((this-name (make-lexer-name name (car rule))))
-                          `(,this-name ()
-                             (make-parser ',this-name
-                               ,@(if (cddr rule)
-                                     `(,(cadr rule)
-                                       (values ,(car rule) (progn ,@(cddr rule))))
-                                   (let ((tmp (gensym)))
-                                     `(((,tmp ,(cadr rule)))
-                                       (values ,(car rule) ,tmp))))))))
+                        (make-lexer-for-terminal name rule))
                     terminals)
          (defparameter ,name
            (make-lexer-struct :name ',name
-             :documentation ,documentation
-             :terminals (list ,@terminal-names)
-             :parser
-             ,(if whitespace
-                  `(make-parser ',name ((:ignore (parse-many ,whitespace)))
-                     (eval-in-context
-                      ,parser-core))
-                parser-core)))))))
+                              :documentation ,documentation
+                              :terminals (list ,@terminal-names)
+                              :parser
+                              ,(if whitespace
+                                   `(make-parser ',name ((:ignore (parse-many ,whitespace)))
+                                                 (eval-in-context
+                                                  ,parser-core))
+                                   parser-core)))))))
 
 (defmacro lex (name &rest args)
   `(eval-parser (lexer-parser ,name) ,@args))
@@ -68,9 +77,9 @@
     (if peeks
         (let ((top (pop peeks)))
           (values-list top))
-      (with-parsed (input-stream)
-                   (((tok val loc) parser))
-                   (values tok val loc)))))
+        (with-parsed (input-stream)
+          (((tok val loc) parser))
+          (values tok val loc)))))
 
 (defmethod get-stream ((s lexer-stream) (ctxt parse-context))
   (multiple-value-bind (tok val loc) (get-stream s nil)
